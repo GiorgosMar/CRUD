482ea78f4e122e291eb1eb12972f0edeb3831eed
Georgios Marinos <95182300+GiorgosMar@users.noreply.github.com>
2022-04-01 17:14:10 +0300

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -12,10 +12,7 @@ app.use(express.json());
 //CREATE 
 app.post("/employee", async (req, res) =>{
     try{
-        const {fName} = req.body;
-        const {lName} = req.body;
-        const {dateOfBirth} = req.body;
-        const {afm} = req.body;
+        const {fName, lName, dateOfBirth, afm} = req.body;
         const newInsert = await pool.query(
             "INSERT INTO employee (firstName, lastName, dateOfBirth, afm) VALUES($1, $2, $3, $4) RETURNING *",
             [fName, lName, dateOfBirth, afm]
@@ -29,22 +26,31 @@ app.post("/employee", async (req, res) =>{
 //GET ALL
 app.get("/employee", async (req, res) =>{
     try{
-        const {afm}= req.query;
-        if(afm === undefined){
-        const allEntries = await pool.query(
-            "SELECT * FROM employee"
-        );
-        res.json(allEntries.rows);}
-        else{
+        const {afm} = req.query;
+        const {page} = req.query;
+        const limit = 5;
+       
+        if(page === undefined && afm === undefined){
+            const countEmpl = await pool.query(
+                "SELECT COUNT(*) FROM employee"
+            );
+            res.json(countEmpl.rows[0].count/limit);
+        }else if(afm === undefined){
+            const startIndex = (page - 1) * limit;
+            const allEnt = await pool.query(
+            "SELECT * FROM employee OFFSET $1 LIMIT $2",[startIndex, limit]
+            );
+            res.json(allEnt.rows);
+        }else{
             const check = await pool.query(
                 "SELECT * FROM employee WHERE afm=$1",
                 [afm]
-            );
-            if(check.rows[0] == undefined){
-                res.json("EMPTY");
-            }else{
-            res.json(check.rows[0]);
-            }
+                );
+                if(check.rows[0] == undefined){
+                    res.json("EMPTY");
+                }else{
+                    res.json(check.rows[0]);
+                }
         }
     }catch (err){
         console.log(err.message);
@@ -66,16 +72,12 @@ app.get("/employee/:id", async (req, res) =>{
 });
 
 
-
 //UPDATE
 app.put("/employee/:id", async (req, res) =>{
     try{
         const {id} = req.params;
        
-        const {fName} = req.body;
-        const {lName} = req.body;
-        const {dateOfBirth} = req.body;
-        const {afm} = req.body;
+        const {fName, lName, dateOfBirth, afm} = req.body;
         const updateElement = await pool.query(
             "UPDATE employee SET firstName=$1, lastName=$2, dateOfBirth=$3, afm=$4 WHERE id = $5",
             [fName, lName, dateOfBirth, afm, id]
