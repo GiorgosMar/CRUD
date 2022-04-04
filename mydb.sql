c330ca45155b32c52d872e40a947eaf53503898e
Georgios Marinos <95182300+GiorgosMar@users.noreply.github.com>
2022-04-04 17:18:49 +0300

    Delete mydb.sql

--- mydb.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE mydb;
-
-CREATE TABLE employee(
-    id SERIAL PRIMARY KEY,
-    firstName VARCHAR(20),
-    lastName VARCHAR(20),
-    dateOfBirth DATE,
-    afm VARCHAR(9) UNIQUE
-);
