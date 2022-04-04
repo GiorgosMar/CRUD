a16d0aae93f51c9ea40de57030e4041d5be305e2
Georgios Marinos <95182300+GiorgosMar@users.noreply.github.com>
2022-04-04 17:17:12 +0300

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -103,6 +103,8 @@ app.delete("/employee/:id", async (req, res) =>{
     }
 });
 
-app.listen(5000, () => {
-    console.log("Server has started on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server has started on port ${PORT}`);
 });
