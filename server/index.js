bdf8e0d43f2c98a1a039782704ccd1278dc398ba
Georgios Marinos <95182300+GiorgosMar@users.noreply.github.com>
2022-04-04 18:01:23 +0300

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
+const path = require('path');
 const express = require("express");
 const app = express();
-const cors = require("cors"); 
+//const cors = require("cors"); 
 const pool = require("./db");
-
-const PORT = process.env.PORT || 5000;
 
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
 
 
-//middleware 
+/*//middleware 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());*/
 
 
 //ROUTES//
