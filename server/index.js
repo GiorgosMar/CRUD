512fce4a9699415208e5650a9551ec053d45c71b
Georgios Marinos <95182300+GiorgosMar@users.noreply.github.com>
2022-04-05 11:41:06 +0300

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,17 +1,12 @@
-const path = require('path');
 const express = require("express");
 const app = express();
-//const cors = require("cors"); 
+const cors = require("cors"); 
 const pool = require("./db");
 
-const PORT = process.env.PORT || 5000;
 
-const buildPath = path.join(__dirname, '..', 'build');
-app.use(express.static(buildPath));
-
-/*//middleware 
+//middleware 
 app.use(cors());
-app.use(express.json());*/
+app.use(express.json());
 
 
 //ROUTES//
@@ -110,6 +105,6 @@ app.delete("/employee/:id", async (req, res) =>{
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server has started on port ${PORT}`);
+app.listen(5000, () => {
+    console.log("Server has started on port 5000");
 });
