d3f8c226b5eb58f3679a72e9c86c303636e9fac5
Georgios Marinos <95182300+GiorgosMar@users.noreply.github.com>
2022-04-05 21:03:05 +0300

    Add files via upload

---
+++ mydb.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE mydb;
+
+CREATE TABLE employee(
+    id SERIAL PRIMARY KEY,
+    firstName VARCHAR(20),
+    lastName VARCHAR(20),
+    dateOfBirth DATE,
+    afm VARCHAR(9) UNIQUE
+);
