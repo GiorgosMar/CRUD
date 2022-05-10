6688c9cff1376bf7f9b43665521b4b8348bfbe6e
Georgios Marinos <95182300+GiorgosMar@users.noreply.github.com>
2022-05-10 18:53:38 +0300

    Add files via upload

---
+++ client/src/components/List.js
@@ -0,0 +1,228 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React, {Fragment, useEffect, useState} from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@mui/material/Button';
+import AddReactionOutlinedIcon from '@mui/icons-material/AddReactionOutlined';
+import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
+import { useNavigate } from "react-router-dom";
+import EditIcon from '@mui/icons-material/Edit';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import Stack from '@mui/material/Stack';
+
+function List({setAuth}) {
+    //navigate//
+    const navigate = useNavigate();
+
+    //useState//
+    const [employees, setEmployees] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [countPages, setCountPages] = useState();
+    const [disableNextButton, setDisableNextButton] = useState(false);
+    const [disablePrevButton, setDisablePrevButton] = useState(false);
+
+
+
+    //handler next page//
+    const handlerNextPage = () => {
+        if(currentPage < countPages){
+            setCurrentPage(prevCurrentPage => prevCurrentPage + 1);
+        }
+    };
+    
+    //handler next button//
+    const handlerNextButton = () => {
+        if(currentPage >= countPages){
+            setDisableNextButton(true);
+        }else{
+            setDisableNextButton(false);
+        }
+    };
+
+    //handler prev page//
+    const handlerPrevPage = () => {
+        if(currentPage>1){
+            setCurrentPage(prevCurrentPage => prevCurrentPage - 1);
+        }
+    };
+
+    //handler prev button//
+    const handlerPrevButton = () => {
+        if(currentPage == 1){
+            setDisablePrevButton(true);
+        }else{
+            setDisablePrevButton(false);
+        }
+    };
+    
+    //Delete confirmation alert//
+    const popupAlert = (id) => {
+        confirmAlert({
+          title: 'Διαγραφή χρήστη',
+          message: 'Είστε σίγουρος/η για την διαγραφή;',
+          buttons: [
+            {
+              label: 'Ακύρωση',
+              onClick: navigate('/')
+            },
+            {
+                label: 'Ναι',
+                onClick: () => deleteEmployee(id)
+            }
+          ]
+        });
+      };
+
+    //Delete//
+    const deleteEmployee = async (id) => {
+        try{
+            const deleteEmpl = await fetch(`/employee/${id}`, {
+                method: "DELETE"
+            }); 
+            getEmployees();
+            navigate('/');
+        } catch(err){
+            console.error(err.message)
+        }
+    }
+    
+    //get employees//
+    const getEmployees = async () => {
+        try {
+            const response = await fetch(`/employee?page=${currentPage}`);
+            const getEmpl = await response.json();
+
+            setEmployees(getEmpl.employees);
+            setCountPages(getEmpl.countPages);
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
+    //useEffect//
+    useEffect(() => {
+        getEmployees();
+        handlerNextButton();
+    }, [currentPage]);
+
+
+    //useEffect//
+    useEffect(() => {
+        handlerNextButton();
+        handlerPrevButton();
+    }, [employees]);
+
+    const logout = async e => {
+        e.preventDefault();
+        try {
+          localStorage.removeItem("token");
+          setAuth(false);
+        } catch (err) {
+          console.error(err.message);
+        }
+      };
+
+    //format date //
+    const getFormattedDate = (dateStr) => {
+        const date = new Date(dateStr);
+        return date.toLocaleDateString('en-GB');
+    }
+    
+    return <Fragment>
+        <p align="left">https://github.com/GiorgosMar/CRUD</p>
+        <h1 align="center">ΛΙΣΤΑ</h1>
+
+        <Stack spacing={2} direction="column">
+        <Stack spacing={113} display="flex" direction="row">
+            <Button
+            variant="outlined" 
+            color="success"
+            size='large' 
+            onClick={() => navigate('/insert')} 
+            startIcon={<AddReactionOutlinedIcon />} 
+            endIcon={<DoubleArrowIcon/>}>ΠΡΟΣΘΗΚΗ
+            </Button>
+            <Button 
+            onClick={e => logout(e)}
+            variant="outlined" 
+            color="error"
+            size='large' 
+            >Αποσύνδεση
+            </Button>
+        </Stack>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650}} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>ΟΝΟΜΑ</TableCell>
+                        <TableCell align="center">ΕΠΩΝΥΜΟ</TableCell>
+                        <TableCell align="center">ΗΜΕΡΟΜΗΝΙΑ ΓΕΝΝΗΣΗΣ</TableCell>
+                        <TableCell align="center">Α.Φ.Μ.</TableCell>
+                        <TableCell align="center">ΕΝΗΜΕΡΩΣΗ</TableCell>
+                        <TableCell align="center" >ΔΙΑΓΡΑΦΗ</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {employees.map((employee) => (
+                        <TableRow
+                            key={employee.id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {employee.firstname}
+                            </TableCell>
+                            <TableCell align="center">{employee.lastname}</TableCell>
+                            <TableCell align="center">{getFormattedDate(employee.dateofbirth)}</TableCell>
+                            <TableCell align="center">{employee.afm}</TableCell>
+                            <TableCell align="center">
+                                <Button
+                                 variant="outlined" 
+                                 color="warning" 
+                                 onClick={() => navigate(`/${employee.id}/update`)} 
+                                 endIcon={<EditIcon/>}>Ενημέρωση</Button>
+                                 </TableCell>
+                            <TableCell 
+                            align="center" >
+                                <Button 
+                                variant="outlined" 
+                                color="error" 
+                                onClick={() => popupAlert(employee.id)}
+                                endIcon={<HighlightOffIcon/>}>ΔΙΑΓΡΑΦΗ</Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            <Typography align="right">
+                <span>Σελίδα {currentPage}η</span>
+                <Button
+                disabled={disablePrevButton}
+                variant="raised"
+                style={{ backgroundColor: 'transparent'}}
+                onClick={() => handlerPrevPage()}
+                ><NavigateBeforeIcon/></Button>
+                <Button
+                disabled={disableNextButton}
+                variant="raised"
+                style={{ backgroundColor: 'transparent'}}
+                onClick={() => handlerNextPage()}
+                ><NavigateNextIcon/></Button>
+            </Typography>
+        </TableContainer>
+        </Stack>
+    </Fragment>;
+}
+
+export default List;
---
+++ client/src/components/Login.js
@@ -0,0 +1,136 @@
+import React, { Fragment, useState } from "react";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Alert from '@mui/material/Alert';
+
+const Login = ({ setAuth }) => {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: ""
+  });
+
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  const { email, password } = inputs;
+
+  const onChangeEmail = e =>
+    setInputs({ ...inputs, email: e.target.value });
+    
+  const onChangePassword = e =>
+    setInputs({ ...inputs, password: e.target.value });
+
+    const onSubmitForm = async e => {
+      e.preventDefault();
+      try {
+        const body = { email, password };
+        const response = await fetch(`/authentication/login`,
+          {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json"
+            },
+            body: JSON.stringify(body)
+          }
+        );
+  
+        const parseRes = await response.json();
+        console.log(parseRes);
+
+       if(parseRes === "Λάθος στοιχεία!"){
+         setErrorMessage("Λάθος στοιχεία!")
+       }else if(parseRes === "Συμπληρώστε τα πεδία"){
+        setErrorMessage("Συμπληρώστε τα πεδία!")
+      }else if(parseRes === "Λάθος Email"){
+        setErrorMessage("Λάθος Email!")
+      }else if (parseRes.jwtToken) {
+        localStorage.setItem("Token", parseRes.jwtToken);
+        setAuth(true);
+      } else {
+        setAuth(false);
+      }
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
+  
+
+  return (
+    <Fragment>
+    <Fragment>
+    <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={onSubmitForm} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              value={inputs.email}
+              onChange={(e) =>
+                setInputs({
+                  ...inputs,
+                  email: e.target.value,
+                })
+              }
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              value={inputs.password}
+              onChange={(e) =>
+                setInputs({
+                  ...inputs,
+                  password: e.target.value,
+                })
+              }
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >Σύνδεση
+            </Button>
+            <Grid>
+              { errorMessage && <Alert severity="error">{errorMessage}</Alert>} 
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </Fragment>
+    </Fragment>
+  );
+};
+
+export default Login;
