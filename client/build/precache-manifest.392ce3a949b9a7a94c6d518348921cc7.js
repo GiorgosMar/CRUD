7a197e7c95830eb9de72e6c4a39f0b208a96fbdb
Georgios Marinos <95182300+GiorgosMar@users.noreply.github.com>
2022-05-10 18:56:31 +0300

    Add files via upload

---
+++ client/build/precache-manifest.392ce3a949b9a7a94c6d518348921cc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4bc3164f52f9697ae4f",
+    "url": "/static/css/main.b941c650.chunk.css"
+  },
+  {
+    "revision": "a4bc3164f52f9697ae4f",
+    "url": "/static/js/main.b892430f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6f5d46c47895077533c5",
+    "url": "/static/css/2.d1bb39a6.chunk.css"
+  },
+  {
+    "revision": "6f5d46c47895077533c5",
+    "url": "/static/js/2.c37d04b2.chunk.js"
+  },
+  {
+    "revision": "4493a8a231497a5de64b50ba016fb81a",
+    "url": "/index.html"
+  }
+];
